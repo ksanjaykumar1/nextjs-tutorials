260b63d9ead67e0582834a63f70a3dcc8728445e
Sanjay <ksanjayk@protonmail.com>
2024-05-22 18:25:43 +0530

    linting added

--- nextjs-tutorial/app/about/page.js
+++ nextjs-tutorial/app/about/page.js
@@ -1,14 +1,5 @@
-import Link from "next/link";
-
-const AboutPage = () => {
-  return (
-    <div>
-      <h1 className='text-7xl'>AboutPage</h1>
-      <Link href='/' className='text-2xl'>
-        home page
-      </Link>
-    </div>
-  );
+const About = () => {
+  return <div>About</div>;
 };
 
-export default AboutPage;
+export default About;
