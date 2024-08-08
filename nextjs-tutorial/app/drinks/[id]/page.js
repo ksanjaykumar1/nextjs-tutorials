d521b3c203d431fa930096c1aa2c1cea4d894295
Sanjay <ksanjayk@protonmail.com>
2024-08-08 13:03:57 +0530

    remote images

--- nextjs-tutorial/app/drinks/[id]/page.js
+++ nextjs-tutorial/app/drinks/[id]/page.js
@@ -1,7 +1,34 @@
-const page = ({ params }) => {
+import Image from 'next/image';
+import Link from 'next/link';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+const getSingleDrink = async (id) => {
+  const res = await fetch(`${url}${id}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch drink...');
+  }
+  return res.json();
+};
+
+const page = async ({ params }) => {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
   return (
     <div>
-      <h1 className='text-7xl'>{params.id}</h1>
+      <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
+        back to drinks
+      </Link>
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className='w-48 h-48 rounded shadow-lg mb-4'
+        priority
+        alt=''
+      />
+      <h1 className='text-4xl mb-8'>{title}</h1>{' '}
     </div>
   );
 };
