dcf11a8422005a7319013639e06d6bdbf59ac5ae
Sanjay <ksanjayk@protonmail.com>
2024-08-17 16:29:57 +0530

    zod library used for validation

--- nextjs-tutorial/utils/action.js
+++ nextjs-tutorial/utils/action.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import prisma from './db';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
 export const getAllTasks = async () => {
   return prisma.task.findMany({
@@ -25,7 +26,11 @@ export const createTask = async (formData) => {
 export const createTaskCustom = async (prevState, formData) => {
   //   await new Promise((resolve) => setTimeout(resolve, 2000));
   const content = formData.get('content');
+  const Task = z.object({
+    content: z.string().min(5),
+  });
   try {
+    Task.parse({ content });
     await prisma.task.create({
       data: {
         content,
@@ -34,6 +39,7 @@ export const createTaskCustom = async (prevState, formData) => {
     revalidatePath('/tasks');
     return { message: 'success !!!' };
   } catch (error) {
+    // console.log(error);
     return { message: 'error...' };
   }
 };
