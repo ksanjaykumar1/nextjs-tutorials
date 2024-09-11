489f2d4e255159a957fa88fceb71367815af5f97
Sanjay <ksanjayk@protonmail.com>
2024-09-11 12:42:46 +0530

    query setup

--- gptgenius/app/providers.js
+++ gptgenius/app/providers.js
@@ -1,10 +1,26 @@
 'use client';
 import { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 export default function Providers({ children }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return (
-    <>
+    <QueryClientProvider>
       <Toaster position='top-center' />
       {children}
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
